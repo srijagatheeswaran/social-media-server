c7a4e583ee7c04ee6a76fc2a10d979d55f4bf05e
Srijagatheeswaran Raja <srijagatheeswaran.raja@dci.in>
2025-07-03 00:20:04 +0530

    cors for

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3001",
+        origin: "*",
         methods: ["GET", "POST"],
         credentials: true,
     },
